5418466f3ac0ded33df470ead6672101d8c3bc72
SalehMohammedSaleh <salehsmns@gmail.com>
2020-12-22 03:41:41 +0300

    First commit

---
+++ SonarDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SonarDemo
+//
+//  Created by Saleh on 20/05/2020.
+//  Copyright © 2020 Saleh Mohammed. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
